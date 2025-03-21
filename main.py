234d4f70c47630d4e25aff4f8faf5e27e16636f2
Moaz Ali <moaaz98@users.noreply.github.com>
2025-03-20 18:55:48 -0700

    Transfer to dict/json

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
 from telegram_bot import send_telegram_message
+import urllib.parse
+import json
+import os
 
 options = Options()
 
@@ -36,37 +39,68 @@ while retry_count < max_retries and not found:
         retry_count += 1
 
 stories = driver.find_elements(By.CLASS_NAME, "load")
+
+def get_id(url):
+    parsed = urllib.parse.urlparse(url)
+    query = urllib.parse.parse_qs(parsed.query)
+    media_encoded = query.get("media", [None])[0]
+    if media_encoded:
+        media_url = urllib.parse.unquote(media_encoded)
+        media_parsed = urllib.parse.urlparse(media_url)
+        media_query = urllib.parse.parse_qs(media_parsed.query)
+
+        ig_key = media_query.get("ig_cache_key", [None])[0]
+        if ig_key:
+            return f"img_{ig_key}"
+
+        vs = media_query.get("vs", [None])[0]
+        if vs:
+            return f"vid_{vs}"
+
+    return None
+
 urls = []
+links = {}
 
 for story in stories:
     try:
         img_src = story.find_element(By.TAG_NAME, "img").get_attribute("src")
+        id = get_id(img_src)
         url = f"http://tinyurl.com/api-create.php?url={img_src}"
-        urls.append(requests.get(url).text)
+        link = requests.get(url).text
+        links[id] = link
     except:
         try:
             video = story.find_element(By.TAG_NAME, "video")
             vid_src = video.find_element(By.TAG_NAME, "source").get_attribute("src")
+            id = get_id(vid_src)
             url = f"http://tinyurl.com/api-create.php?url={vid_src}"
-            urls.append(requests.get(url).text)
+            link = requests.get(url).text
+            links[id] = link
         except:
-            urls.append(None)
+            pass
 
 driver.quit()
 
-existing_urls = set()
+existing_links = {}
+if os.path.exists("links.json"):
+    try:
+        with open("links.json", "r", encoding="utf-8") as f:
+            existing_links = json.load(f)
+    except json.JSONDecodeError:
+        pass
+
+new_links = []
+
+for id, link in links.items():
+    if id not in existing_links:
+        new_links.append(link)
+        existing_links[id] = link
+
+send_telegram_message(new_links)
+
 try:
-    with open('urls.txt', 'r', encoding='utf-8') as f:
-        existing_urls = set(line.strip() for line in f)
-except FileNotFoundError:
+    with open("links.json", "w", encoding="utf-8") as f:
+        json.dump(existing_links, f, indent=2)
+except:
     pass
-
-new_urls = [url for url in urls if url not in existing_urls]
-
-print(new_urls)
-
-send_telegram_message(new_urls)
-
-with open('urls.txt', 'a', encoding='utf-8') as f:
-    for url in new_urls:
-        f.write(f"{url}\n")
