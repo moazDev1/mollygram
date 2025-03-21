497b138b3cf9fa3f58b9647c4d1d37e05f19a49a
Moaz Ali <moaaz98@users.noreply.github.com>
2025-03-20 21:37:48 -0700

    Fix

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
@@ -19,7 +20,8 @@ while True:
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
 
-    driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
+    service = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(service=service, options=options)
     driver.get('https://mollygram.com/')
 
     search_input = driver.find_element(By.ID, "link")
